b32e265979b325576f33530b62a7a20e8d053a56
icmsdev <master@icmsdev.com>
2018-05-24 16:52:08 +0800

    [F]修复多处细节问题 [A]增加同一文件上传检测开关.默认开启状态

--- app/func/template/tpl.modifier.call.php
+++ app/func/template/tpl.modifier.call.php
@@ -16,13 +16,16 @@
  */
 function tpl_modifier_call($object,$method){
     $arg_list = func_get_args ();
+    //{参数0|call:'class::method':'参数1':'参数2':'参数3'}
     if(strpos($method,'::')!==false){
         $array = array_merge((array)$object,array_slice($arg_list, 2));
         $call  = $method;
     }else if(!is_object($object) && strpos($object,'::')!==false){
+        //{'class::method'|call:'method':'参数1':'参数2':'参数3'}
         $array = array_slice($arg_list, 1);
         $call  = $object;
     }else {
+        //{object|call:'method':'参数1':'参数2':'参数3'}
         $array = array_slice($arg_list, 2);
         $call  = array($object,$method);
     }
