35df11f86382effe5b451f5858b2927fd747db9d
icmsdev <master@icmsdev.com>
2018-07-19 12:40:45 +0800

    [U]新增 `文章模板标签` 分页模式下 total="指定显示文章数" 或 pageNum="指定显示页数"

--- app/article/article.func.php
+++ app/article/article.func.php
@@ -128,10 +128,27 @@ class articleFunc{
 		$offset = (int)$vars['offset'];
 		if ($vars['page']) {
 			$total_type = $vars['total_cache'] ? 'G' : null;
-			$total      = iCMS::page_total_cache("SELECT count(*) FROM `#iCMS@__article` {$where_sql}", $total_type,iCMS::$config['cache']['page_total']);
+			$total      = (int)$vars['total'];
+			if(isset($vars['pageNum'])){
+				$total = (int)$vars['pageNum']*$maxperpage;
+			}
+			if(!isset($vars['total']) && !isset($vars['pageNum'])){
+				$total = iCMS::page_total_cache(
+					"SELECT count(*) FROM `#iCMS@__article` {$where_sql}",
+					$total_type,
+					iCMS::$config['cache']['page_total']
+				);
+			}
+
 			$pagenav    = isset($vars['pagenav']) ? $vars['pagenav'] : "pagenav";
 			$pnstyle    = isset($vars['pnstyle']) ? $vars['pnstyle'] : 0;
-			$multi      = iUI::page(array('total_type' => $total_type, 'total' => $total, 'perpage' => $maxperpage, 'unit' => iUI::lang('iCMS:page:list'), 'nowindex' => $GLOBALS['page']));
+			$multi      = iUI::page(array(
+				'total_type' => $total_type,
+				'total'      => $total,
+				'perpage'    => $maxperpage,
+				'unit'       => iUI::lang('iCMS:page:list'),
+				'nowindex'   => $GLOBALS['page'])
+			);
 			$offset     = $multi->offset;
 			iView::assign("article_list_total", $total);
 		}
