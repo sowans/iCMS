36b09682445b74f5699e75e5e8d211e7ae0437f2
icmsdev <master@icmsdev.com>
2018-08-02 21:30:54 +0800

    [U]更新 `htmlpurifier` 到 4.10.0

--- core/git.version.php
+++ core/git.version.php
@@ -7,7 +7,7 @@
 * @site https://www.icmsdev.com
 * @licence https://www.icmsdev.com/LICENSE.html
 */
-define('GIT_COMMIT', '04c1310e7d4624b445e589a6c4e8120bc9a9c5ad');
+define('GIT_COMMIT', '62de04e57a67f2690dbf88b7d381af61a0969ef3');
 define('GIT_AUTHOR', 'icmsdev');
 define('GIT_EMAIL', 'master@icmsdev.com');
-define('GIT_TIME', '1533181586');
+define('GIT_TIME', '1533203504');
