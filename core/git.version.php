05050f5ac004a2688f84cf636d3dc32f765c42a1
icmsdev <master@icmsdev.com>
2018-10-17 10:23:07 +0800

    [F]修复 `前端` jquery引用错误

--- core/git.version.php
+++ core/git.version.php
@@ -7,7 +7,7 @@
 * @site https://www.icmsdev.com
 * @licence https://www.icmsdev.com/LICENSE.html
 */
-define('GIT_COMMIT', '3bb6342abf6de473b5ef73eaef45e0e45550ab3d');
+define('GIT_COMMIT', '188593d9adab3cc0ae085155fa1de3813538efe2');
 define('GIT_AUTHOR', 'icmsdev');
 define('GIT_EMAIL', 'master@icmsdev.com');
-define('GIT_TIME', '1539405439');
+define('GIT_TIME', '1539424449');
